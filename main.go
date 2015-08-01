57bab5a8ecdf9cef8535373b58893a6c2455a9b5
Tom Steele <thomasjsteele@gmail.com>
2015-07-31 17:31:04 -0700

    Setup for new API

--- main.go
+++ main.go
@@ -1,101 +1,93 @@
 package main
 
 import (
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/url"
 	"os"
+	"strings"
 
-	"github.com/lair-framework/api-server/client"
 	"github.com/lair-framework/go-lair"
 	"github.com/lair-framework/go-nmap"
 )
 
 const (
-	TOOL     = "Nmap"
-	OSWEIGHT = 50
+	version  = "2.0.0"
+	tool     = "nmap"
+	osWeight = 50
+	usage    = `
+Usage:
+  drone-nmap <id> <filename>
+  export LAIR_ID=<id>; drone-nmap <filename>
+Options:
+  -v              show version and exit
+  -h              show usage and exit
+  -k              allow insecure SSL connections
+  -force-ports    disable data protection in the API server for excessive ports
+  -tags           a comma separated list of tags to add to every host that is imported
+`
 )
 
-const usage = `
-	Usage: nmap.go <project_id> <file>
-`
-
-func buildProject(run *nmap.NmapRun, projectId string) (*lair.Project, error) {
+func buildProject(run *nmap.NmapRun, projectID string, tags []string) (*lair.Project, error) {
 	project := &lair.Project{}
-	project.Id = projectId
-	project.Tool = TOOL
-	project.Commands = append(project.Commands, lair.Command{Tool: TOOL, Command: run.Args})
+	project.ID = projectID
+	project.Tool = tool
+	project.Commands = append(project.Commands, lair.Command{Tool: tool, Command: run.Args})
 
-	// Loop through all hosts
 	for _, h := range run.Hosts {
-		host := &lair.Host{}
+		host := &lair.Host{Tags: tags}
 		if h.Status.State != "up" {
-			// Don't import dead hosts
 			continue
 		}
-		host.Alive = true
 
-		// Find IP address and Mac Address
 		for _, address := range h.Addresses {
 			switch {
 			case address.AddrType == "ipv4":
-				host.StringAddr = address.Addr
+				host.IPv4 = address.Addr
 			case address.AddrType == "mac":
-				host.MacAddr = address.Addr
+				host.MAC = address.Addr
 			}
 		}
 
-		// Find all hostnames
 		for _, hostname := range h.Hostnames {
 			host.Hostnames = append(host.Hostnames, hostname.Name)
 		}
 
-		// Find all ports
 		for _, p := range h.Ports {
-			port := &lair.Port{}
-			port.Port = p.PortId
-			port.Protocol = p.Protocol
+			service := lair.Service{}
+			service.Port = p.PortId
+			service.Protocol = p.Protocol
 
-			// Find the port status
 			if p.State.State != "open" {
-				// only import open ports
 				continue
 			}
-			port.Alive = true
 
-			// Find service and product
 			if p.Service.Name != "" {
-				port.Service = p.Service.Name
-				port.Product = "unknown" // Default value
+				service.Service = p.Service.Name
+				service.Product = "Unknown"
 				if p.Service.Product != "" {
-					// Set product name
-					port.Product = p.Service.Product
+					service.Product = p.Service.Product
 					if p.Service.Version != "" {
-						// Append version
-						port.Product += " " + p.Service.Version
+						service.Product += " " + p.Service.Version
 					}
 				}
 			}
 
-			// Add any NSE script output
 			for _, script := range p.Scripts {
-				note := &lair.Note{Title: script.Id, Content: script.Output, LastModifiedBy: TOOL}
-				port.Notes = append(port.Notes, *note)
+				note := &lair.Note{Title: script.Id, Content: script.Output, LastModifiedBy: tool}
+				service.Notes = append(service.Notes, *note)
 			}
 
-			host.Ports = append(host.Ports, *port)
+			host.Services = append(host.Services, service)
 		}
 
-		// Find the operating system
 		if len(h.Os.OsMatch) > 0 {
-			os := &lair.OS{}
-			os.Tool = TOOL
-			os.Weight = OSWEIGHT
+			os := lair.OS{}
+			os.Tool = tool
+			os.Weight = osWeight
 			os.Fingerprint = h.Os.OsMatch[0].Name
-			host.OS = append(host.OS, *os)
+			host.OS = os
 		}
 
 		project.Hosts = append(project.Hosts, *host)
@@ -106,71 +98,46 @@ func buildProject(run *nmap.NmapRun, projectId string) (*lair.Project, error) {
 }
 
 func main() {
-
-	// Parse command line args
-	flag.Usage = func() { fmt.Print(usage) }
+	showVersion := flag.Bool("v", false, "")
+	insecureSSL := flag.Bool("k", false, "")
+	forcePorts := flag.Bool("force-ports", false, "")
+	tags := flag.String("tags", "", "")
+	flag.Usage = func() {
+		fmt.Println(usage)
+	}
 	flag.Parse()
-	if len(flag.Args()) != 2 {
-		log.Fatal("You need to supply the Lair project ID and file you wish to import")
+	if *showVersion {
+		log.Println(version)
+		os.Exit(0)
 	}
-	pid := flag.Arg(0)
-	f := flag.Arg(1)
-
-	// Parse and setup to target drone server info
-	dest := os.Getenv("LAIR_API_SERVER")
-	if dest == "" {
-		log.Fatal("Missing LAIR_API_SERVER environment variable.")
+	lairURL := os.Getenv("LAIR_API_SERVER")
+	if lairURL == "" {
+		log.Fatal("Fatal: Missing LAIR_API_SERVER environment variable")
 	}
-	u, err := url.Parse(dest)
+	lairPID := os.Getenv("LAIR_ID")
+	var filename string
+	switch len(flag.Args()) {
+	case 2:
+		lairPID = flag.Arg(0)
+		filename = flag.Arg(1)
+	case 1:
+		filename = flag.Arg(0)
+	default:
+		log.Fatal("Fatal: Missing required argument")
+	}
+	log.Println(lairPID, filename, *insecureSSL, *forcePorts, *tags)
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatalf("Fatal: Could not open file. Error %s", err.Error())
+	}
+	hostTags := strings.Split(*tags, ",")
+	nmapRun, err := nmap.Parse(data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if u.User == nil {
-		log.Fatal("Missing username and/or password")
-	}
-	user := u.User.Username()
-	pass, _ := u.User.Password()
-	if user == "" || pass == "" {
-		log.Fatal("Missing username and/or password")
-	}
-	target := &client.LairTarget{User: user, Password: pass, Host: u.Host}
-
-	// Read the raw data file and parse
-	buf, err := ioutil.ReadFile(f)
+	project, err := buildProject(nmapRun, lairPID, hostTags)
 	if err != nil {
 		log.Fatal(err)
 	}
-	nmapRun, err := nmap.Parse(buf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Convert the Nessus structs to a go-lair-drone project
-	project, err := buildProject(nmapRun, pid)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Import the project into Lair
-	res, err := client.ImportProject(target, project)
-	if err != nil {
-		log.Fatal("Unable to import project: ", err)
-	}
-	defer res.Body.Close()
-
-	// Inspect the reponse
-	droneRes := &client.Response{}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := json.Unmarshal(body, droneRes); err != nil {
-		log.Fatal(err)
-	}
-	if droneRes.Status == "Error" {
-		log.Fatal("Import failed : ", droneRes.Message)
-	} else {
-		log.Println("Import complete, status : ", droneRes.Status)
-	}
-
+	log.Println(project)
 }
